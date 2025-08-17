ee559734d26383957fd6a2d0ecf7471703ee3a7c
VDV <beepper210980@gmail.com>
2025-08-17 13:00:07 +0300

    Добавлены модели Role, Permission  и соответстующие таблицы в БД

---
+++ FlowEvents/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowEvents.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string Description { get; set; }
+        public List<Permission> Permissions { get; set; } = new List<Permission>();
+    }
+}
